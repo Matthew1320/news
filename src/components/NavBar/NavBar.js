c751ab1b06b30e5145327dd77f6f2f6ab74aba4d
Matthew1320 <miglovesbaseball@gmail.com>
2024-06-22 07:45:24 -0400

    navbar customized

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -5,9 +5,13 @@ import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 const Menu = () => (
   <>
     <p><a href='#home'>Home</a></p>
-    <p><a href='#courses'>Courses</a></p>
-    <p><a href='#projects'>Projects</a></p>
-    <p><a href='#contact'>Contact</a></p>
+    <p><a href='#business'>Business</a></p>
+    <p><a href='#entertainment'>Entertainment</a></p>
+    <p><a href='#general'>General</a></p>
+    <p><a href='#health'>Health</a></p>
+    <p><a href='#science'>Science</a></p>
+    <p><a href='#sports'>Sports</a></p>
+    <p><a href='#technology'>Technology</a></p>
   </>
 )
 
